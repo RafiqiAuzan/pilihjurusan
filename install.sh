338b49c652789458048bbb12dc5bd01570aaf6b0
Azvya Erstevan I <erstevn@gmail.com>
2022-11-07 14:08:03 +0700

    Added install sh and documentation folder

--- install.sh
+++ install.sh
@@ -2,5 +2,8 @@ docker run --rm \
     -u "$(id -u):$(id -g)" \
     -v $(pwd):/var/www/html \
     -w /var/www/html \
-    erstevn/sail-php:latest \
+    laravelsail/php81-composer:latest \
     composer install --ignore-platform-reqs
+cp ./docker/php/config/opcache.ini-example ./docker/php/config/opcache.ini
+cp ./docker/php/config/xdebug.ini-example ./docker/php/config/xdebug.ini
+echo "xdebug.client_host=$(hostname -I)" >>./docker/php/config/xdebug.ini
