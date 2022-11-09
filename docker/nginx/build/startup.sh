1904b6a6817e2737ca67e7974c8a7912735888c2
Azvya Erstevan I <erstevn@gmail.com>
2022-11-09 23:16:23 +0700

    Reconfigured docker

--- docker/nginx/build/startup.sh
+++ docker/nginx/build/startup.sh
@@ -1,42 +1,52 @@
 #!/bin/bash
 
-app_env=$(cat /var/www/html/.env | grep APP_ENV | cut -d "=" -f2)
-app_ssl=$(cat /var/www/html/.env | grep APP_SSL | cut -d "=" -f2)
-server_name=$(cat /var/www/html/.env | grep APP_URL | cut -d "=" -f2)
-server_name="${server_name/https:\/\//""}"
-server_name="${server_name/http:\/\//""}"
+APP_ENV=$(grep 'APP_ENV' /var/www/html/.env | sed 's/^.*=//') \
+APP_SSL=$(grep 'APP_SSL' /var/www/html/.env | sed 's/^.*=//') \
+SERVER_NAME=$(grep 'APP_URL' /var/www/html/.env | sed 's/^.*\///')
 
-if [ "$app_env" != "production" ]; then
-    if [ ! -f /etc/nginx/ssl/$server_name.crt ]; then
-        openssl req -x509 -nodes -days 365 -subj "/CN=Pilih Jurusan CA/OU=Pilih Jurusan IT/O=Pilih Jurusan/L=Jakarta Selatan/C=ID" -addext "subjectAltName=DNS.1:localhost,DNS.2:$server_name" -newkey rsa:2048 -keyout /etc/nginx/ssl/$server_name.key -out /etc/nginx/ssl/$server_name.crt
+if [ "$APP_ENV" == "local" ]; then
+    if [ ! -f /etc/nginx/ssl/$SERVER_NAME.crt ]; then
+        openssl req -x509 -nodes -days 365 -subj "/CN=Pilih Jurusan CA/OU=Pilih Jurusan IT/O=Pilih Jurusan/L=Jakarta Selatan/C=ID" -addext "subjectAltName=DNS.1:localhost,DNS.2:$SERVER_NAME" -newkey rsa:2048 -keyout /etc/nginx/ssl/$SERVER_NAME.key -out /etc/nginx/ssl/$SERVER_NAME.crt
     fi
 
     # create a server name based on .env file
-    (cd /etc/nginx/sites-available && cp laravel.conf-example laravel.conf && sed -i "s/\${SERVER_NAME}/$server_name/" laravel.conf)
+    (cd /etc/nginx/sites-available && cp pilihjurusan.conf-example pilihjurusan.conf && sed -i "s/\${SERVER_NAME}/$SERVER_NAME/" pilihjurusan.conf)
+else
+    if [ ! -f /etc/nginx/ssl/$SERVER_NAME.crt ]; then
+        certbot --nginx -d $SERVER_NAME -d www.$SERVER_NAME -n --agree-tos --email abdul.arfan@gmail.com
+    fi
+    # create a server name based on .env file
+    (cd /etc/nginx/sites-available && cp pilihjurusan.conf-example pilihjurusan.conf && sed -i "s/\${SERVER_NAME}/$SERVER_NAME www.$SERVER_NAME/" pilihjurusan.conf)
 fi
 
-if [ "$app_ssl" == "secure" ]; then
+if [ "$APP_SSL" == "secure" ]; then
     echo "server {
             listen 80 default_server;
             listen [::]:80 default_server;
-            server_name _ $server_name;
+            server_name _ $SERVER_NAME;
             return 301 https://\$host\$request_uri;
         }" >/etc/nginx/conf.d/redirect.conf
 else
     echo "" >/etc/nginx/conf.d/redirect.conf
 fi
 
-# cron job to restart nginx every 6 hour
+# cron job to restart nginx every 00:00:00 UTC +7
 (
     crontab -l
-    echo "0 0 */4 * * nginx -s reload"
+    echo "0 17 */4 * * nginx -s reload"
 ) | crontab -
 
+if [ "$APP_ENV" != "local" ]; then
+    (
+        crontab -l
+        echo "30 16 */4 * * certbot renew"
+    ) | crontab -
+fi
 # Start crond in background
 crond -l 2 -b
 
 #* * * * * root nginx -s reload >> /var/log/cron.log
 
 # Start nginx in foreground
-echo "NGINX started, daemon will restart every 6 hours now."
+echo "NGINX started, daemon will restart every 00:00:00 UTC +7 now."
 nginx
