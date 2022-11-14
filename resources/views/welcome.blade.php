87b8f333a549eeb98e3f3f84524c8577fddef0a6
Azvya Erstevan I <erstevn@gmail.com>
2022-11-14 09:24:50 +0700

    Updated styles

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,7 @@
 <html>
 
 <head>
+    @vite('resources/css/app.css')
     @livewireStyles(['nonce' => csp_nonce()])
 </head>
 
@@ -8,8 +9,8 @@
     <h1 class="text-8xl">Hello World!</h1>
     <a class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
         href="/login">Login</a>
+    @vite('resources/js/app.js')
     @livewireScripts(['nonce' => csp_nonce()])
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </body>
 
 </html>
