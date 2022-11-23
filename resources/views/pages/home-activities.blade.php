f9748daa65fd9b1d3d725bbedaee8ab4979db6c6
rafiakbar13 <muhammadrafia13@gmail.com>
2022-11-23 21:54:21 +0700

    added view activities

---
+++ resources/views/pages/home-activities.blade.php
@@ -0,0 +1,9 @@
+{{-- Activties section --}}
+
+<section id= "aktivitas "class="w-full">
+    <div class="mx-auto">
+        <h3 class="text-4xl font-bold text-center font-head">Yang terbaru dari kami</h3>
+        <p class="text-lg font-medium text-center font-head">Cerita baru, kegiatan, dan keseruan lainnya, Baca Blog terbaru kami disini.</p>
+    </div>
+
+</section>
