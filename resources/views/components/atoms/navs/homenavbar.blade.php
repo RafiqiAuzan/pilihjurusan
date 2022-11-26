f2c774e42d7bba03ac5962b12805462c7ff41013
Oktarinia10 <rossa101410@gmail.com>
2022-11-27 00:06:11 +0700

    update navbar

--- resources/views/components/atoms/navs/homenavbar.blade.php
+++ resources/views/components/atoms/navs/homenavbar.blade.php
@@ -15,15 +15,19 @@
       </div>
       
       <div :class="{'hidden lg:block' : !navmenu}">
-        <div class="justify-center gap-4 text-center lg:flex lg:items-center">
-          <ul class="gap-4  lg:flex">
-            <li><a class="transition-all hover:underline hover:decoration-primary-500 hover:underline-offset-2 hover:font-semibold" href="#">Beranda</a></li>
-            <li><a class="transition-all hover:underline hover:decoration-primary-500 hover:underline-offset-2 hover:font-semibold" href="#">Layanan</a></li>
-            <li><a class="transition-all hover:underline hover:decoration-primary-500 hover:underline-offset-2 hover:font-semibold" href="#">Artikel</a></li>
-            <li><a class="transition-all hover:underline hover:decoration-primary-500 hover:underline-offset-2 hover:font-semibold" href="#">Tentang</a></li>
+        <div class="justify-center text-center gap-8 lg:flex lg:items-center">
+          <ul class="gap-5 lg:flex">
+            <li><a class="mx-5 transition-all hover:underline hover:decoration-primary-500 hover:underline-offset-2 hover:font-semibold" href="#">Beranda</a></li>
+            <li><a class="mx-5 transition-all hover:underline hover:decoration-primary-500 hover:underline-offset-2 hover:font-semibold" href="#">Layanan</a></li>
+            <li><a class="mx-5 transition-all hover:underline hover:decoration-primary-500 hover:underline-offset-2 hover:font-semibold" href="#">Artikel</a></li>
+            <li><a class="mx-5 transition-all hover:underline hover:decoration-primary-500 hover:underline-offset-2 hover:font-semibold" href="#">Tentang</a></li>
+            <li class="text-center transition-all hover:scale-90"> 
+              <x-atoms.button size="sm" status="outline" class="mx-auto text-base">
+              Login
+          </x-atoms.button></li>
           </ul>
           
-          <x-atoms.button size="sm" status="outline"> Login</x-atoms.button>
+          
         </div>
       </div>
     </div>
