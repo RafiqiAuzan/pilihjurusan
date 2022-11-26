280563247cb9bab86b0f9166389f7e88116983c9
rafiakbar13 <muhammadrafia13@gmail.com>
2022-11-26 14:01:01 +0700

    added pages activities in landing pages

--- resources/views/components/layouts/full-rows.blade.php
+++ resources/views/components/layouts/full-rows.blade.php
@@ -1,6 +1,6 @@
 @props(['class' => ''])
 
 <x-layouts.base class=" {{ $class }}">
-    <h1 class="text-2xl">Landing</h1>
+    {{-- <h1 class="text-2xl">Landing</h1> --}}
     {{ $slot }}
 </x-layouts.base>
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('pages.home');
+// });
+
 Route::get('/', function () {
     return view('pages.home-activities');
 });
