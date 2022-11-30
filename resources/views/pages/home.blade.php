a3fff4cae2eeda6757146f470db7f3c571232ef4
rafiakbar13 <muhammadrafia13@gmail.com>
2022-11-30 12:16:12 +0700

    merge branch hero

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -8,23 +8,28 @@
         <div>
             <div class="flex items-center min-h-[37rem]">
                 <div class="content-center grow">
-                    <h1 class="py-4 text-xl md:text-6xl"><strong>#PilihYangCocok</strong></h1>
-                    <p class="text-base-600 max-w-[35rem]">Bersama Pilih Jurusan yang siap membantu kamu memilih jurusan dan karier sesuai minat dan bakatmu.</p>
-                    <x-atoms.button class="mt-7"> Coba Gratis</x-atoms.button>
+                    <h1 class="font-display font-extrabold py-4 text-xl md:text-7xl"><strong>#PilihYangCocok</strong></h1>
+                    <p class="font-body font-normal text-base-600 max-w-[35rem]">Bersama Pilih Jurusan yang siap membantu kamu memilih jurusan dan karier sesuai minat dan bakatmu.</p>
+                    <x-atoms.button class="bi bi-arrow-right-circle-fill mt-7"> Coba Gratis</x-atoms.button>
                 </div>
                 <div class="">
-                    <img src="{{ asset('img/landing/hero.svg')}}" alt="">
+                    <img class="" src="{{ URL::to('/img/landing/hero.svg') }}">
                 </div>
             </div>
         </div>
-    </section>
+    </div>
+</section>
 
-    {{-- Promo Card --}}
-    <section class="bg-base-100">
-        <div class="container py-10 mx-auto">
-            <div class="bg-light-100 rounded-[1.5rem] text-center p-10 py-24"><strong class="text-xl">Promo Card</strong></div>
+{{-- Promo Card --}}
+
+<section class="bg-base-100">
+    <div class="container py-10 px-20 mx-auto ">
+        <div class=" container mx-auto flex justify-between text-center bg-gradient-to-r from-base-100 via-warning/25 to-base-100 rounded-[1.5rem] p-10 py-14">
+            <strong class="text-xl mx-auto">Ayo dapatkan tawaran menarik dari kami!</strong>
+            <button class="font-body font-bold text-xl  mx-auto text-primary-500">50% Discount</button>
         </div>
-    </section>
+    </div>
+</section>
 
 
 {{-- Section benefit --}}
@@ -261,17 +266,17 @@
               </div>
             <div class="container p-4 mx-auto">
                 <div class="grid grid-cols-1 pt-5 md:grid-cols-2 xl:grid-cols-3 px-14">
-            <div class="p-3" border-solid shadow-lg p-9 rounded-3xl bg-light-100 border-light-300>
+            <div class="p-3  border-solid shadow-lg p-9 rounded-3xl bg-light-100 border-light-300" >
                 <div class="mb-5">
                     <img src="{{ asset('img/landing/card-1.png')}}" alt="Frame">
                 </div>
             </div>               
-            <div class="p-3" border-solid shadow-lg p-9 rounded-3xl bg-light-100 border-light-300>
+            <div class="p-3 border-solid shadow-lg p-9 rounded-3xl bg-light-100 border-light-300" >
                 <div class="mb-5">
                     <img src="{{ asset('img/landing/card-2.png')}}" alt="Frame">
                 </div>
             </div>
-            <div class="p-3" border-solid shadow-lg p-9 rounded-3xl bg-light-100 border-light-300>
+            <div class="p-3 border-solid shadow-lg p-9 rounded-3xl bg-light-100 border-light-300" >
                 <div class="mb-3">
                     <img src="{{ asset('img/landing/card-3.png')}}" alt="Frame">
                 </div>
@@ -289,27 +294,27 @@
                   <h2 class="font-normal text-base-500">Layanan kami telah digunakan oleh banyak mitra</h2>
               </div>
                 <div class="grid grid-cols-1 pt-5 md:grid-cols-2 xl:grid-cols-5 px-14">
-            <div class="p-3" border-solid shadow-lg p-9 rounded-3xl bg-light-100 border-light-300>
+            <div class="p-3 border-solid shadow-lg p-9 rounded-3xl bg-light-100 border-light-300" >
                 <div class="mb-5">
                     <img src="{{ asset('img/landing/card-partner1.png')}}" alt="Frame">
                 </div>  
             </div>
-            <div class="p-3" border-solid shadow-lg p-9 rounded-3xl bg-light-100 border-light-300>
+            <div class="p-3 border-solid shadow-lg p-9 rounded-3xl bg-light-100 border-light-300" >
                 <div class="mb-5">
                     <img src="{{ asset('img/landing/card-partner2.png')}}" alt="Frame">
                 </div>  
             </div>
-            <div class="p-3" border-solid shadow-lg p-9 rounded-3xl bg-light-100 border-light-300>
+            <div class="p-3 border-solid shadow-lg p-9 rounded-3xl bg-light-100 border-light-300" >
                 <div class="mb-5">
                     <img src="{{ asset('img/landing/card-partner1.png')}}" alt="Frame">
                 </div>  
             </div>
-            <div class="p-3" border-solid shadow-lg p-9 rounded-3xl bg-light-100 border-light-300>
+            <div class="p-3 border-solid shadow-lg p-9 rounded-3xl bg-light-100 border-light-300" >
                 <div class="mb-5">
                     <img src="{{ asset('img/landing/card-partner2.png')}}" alt="Frame">
                 </div>  
             </div>
-            <div class="p-3" border-solid shadow-lg p-9 rounded-3xl bg-light-100 border-light-300>
+            <div class="p-3 border-solid shadow-lg p-9 rounded-3xl bg-light-100 border-light-300" >
                 <div class="mb-5">
                     <img src="{{ asset('img/landing/card-partner3.png')}}" alt="Frame">
                 </div>  
