0cf4a3f8c05cc669c7b7d0032dba0ac99e785e32
zenadwim <zenadwimentari@gmail.com>
2022-12-01 13:30:37 +0700

    edit message landing

--- resources/views/components/atoms/youtube.blade.php
+++ resources/views/components/atoms/youtube.blade.php
@@ -1,17 +1,21 @@
-<iframe 
-    width="1024px" height="540px" class="z-0 rounded-[36px]"
-    src="{{ $src }}" id="youtube"
-    title="YouTube video player" frameborder="0" 
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-    allowfullscreen>
-</iframe>
-<button 
-    id="playbtn" class="playbtn z-1 rounded-[42px] flex flex-col items-start 
-    px-[19px] py-[18px] w-[83px] h-[84px] absolute bg-[#FFFFFF]">
-    <svg xmlns="http://www.w3.org/2000/svg" width="46" height="47" fill="#F06623" class="bi bi-play-fill" viewBox="0 0 16 16">
-        <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z"/>
-    </svg>
-</button>
+<div class="flex flex-col justify-center items-center isolate max-w-screen-lg lg:h-full h-60 lg:w-full w-96">
+    {{-- lg:h-full h-60 lg:w-full w-96 --}}
+    <iframe
+        width="100%" height="540px" 
+        class="z-0 rounded-[36px]"
+        src="{{ $src }}" id="youtube"
+        title="YouTube video player" frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+        allowfullscreen>
+    </iframe>
+    <button 
+        id="playbtn" class="playbtn z-1 rounded-[42px] flex flex-col items-start 
+        px-[19px] py-[18px] w-[83px] h-[84px] absolute bg-[#FFFFFF]">
+        <svg xmlns="http://www.w3.org/2000/svg" width="46" height="47" fill="#F06623" class="bi bi-play-fill" viewBox="0 0 16 16">
+            <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z"/>
+        </svg>
+    </button>
+</div>
 
 <script type="text/javascript">
 $(document).ready(function(){
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -169,25 +169,18 @@
 
 {{-- Section Message --}}
     <section>
-        <div class="flex flex-col justify-center items-center py-32 gap-12
-        bg-[#F9F7EA]" px="100px">
-            <div class="flex flex-col items-center max-w-5xl" height="681px">
-                <div class="flex flex-col justify-center items-center gap-12 max-w-5xl" height="681px">
-                    <div class="flex flex-col items-center gap-6 max-w-5xl" height="93px">
-                        <p class="max-w-5xl h-11 font-display not-italic font-bold text-4xl text-center text-[#262829]">
-                            Seperti apa sistem Bimbingannya?
+        <div class="flex flex-col justify-center items-center py-32 gap-12" px="100px">
+                <div class="flex flex-col justify-center items-center gap-12">
+                    <div class="items-center gap-6">
+                        <p class="font-display text-4xl pb-5 font-bold text-center text-base-800 mx-auto lg:w-full w-9/12">
+                            {{ __('Seperti apa sistem Bimbingannya?') }}
                         </p>
-                        <p class="max-w-5xl h-11 font-body not-italic font-normal text-base text-center text-[#6D6C63]">
-                            sistem bimbingan insentif akan sangat membantu
+                        <p class="lg:block hidden max-w-5xl h-11 font-body not-italic font-normal text-base text-center text-base-600">
+                            {{ __('sistem bimbingan insentif akan sangat membantu') }}
                         </p>
                     </div>
-                    <div class="flex flex-col items-start gap-5 max-w-5xl" height="540px">
-                        <div class="flex flex-col justify-center items-center isolate max-w-5xl" height="540px">
-                            <x-atoms.youtube src="https://www.youtube.com/embed/8Ea4oq8qFtM" />
-                        </div>
-                    </div>
+                    <x-atoms.youtube src="https://www.youtube.com/embed/8Ea4oq8qFtM" />
                 </div>
-            </div>
         </div>
     </section>
 
