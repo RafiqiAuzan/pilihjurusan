18ff3448bf6015b3c1c8f3068bad8e3b6b93614b
ishamadway <mochamad.isha@gmail.com>
2022-12-01 14:47:08 +0700

    New update

--- resources/views/components/layouts/full-row.blade.php
+++ resources/views/components/layouts/full-row.blade.php
@@ -1,5 +1,18 @@
 @props(['class' => ''])
 
-<x-layouts.base class="{{ $class }}">
-    {{ $slot }}
-</x-layouts.base>
+<x-layouts.base class=" {{ $class }}">
+
+    <x-atoms.navs.homenavbar/>
+
+    <div class="">
+      <div class="min-h-screen">
+      {{ $slot }}
+      </div>
+    </div>
+
+    {{-- widget --}}
+    <x-atoms.cornerwidget/>
+
+
+    {{-- <x-atoms.navs.homefooter/> --}}
+  </x-layouts.base>
