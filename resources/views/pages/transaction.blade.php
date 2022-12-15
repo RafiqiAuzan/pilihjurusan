7e9c6249b83130658aec48b6f9d7d281715434d2
rafiakbar13 <muhammadrafia13@gmail.com>
2022-12-15 14:57:11 +0700

    added view transaction

--- resources/views/pages/transaction.blade.php
+++ resources/views/pages/transaction.blade.php
@@ -2,7 +2,7 @@
 
     <!-- component -->
     <section id="transaction" class="mx-auto">
-        <div class="container w-[1280px] mx-auto px-7 lg:px-[128px]">
+        <div class="container w-full mx-auto px-7 lg:px-[128px]">
             <nav class=" rounded-md w-full mt-[100px] items-center">
                 <ol class="list-reset flex text-xs text-base-400">
                     <li><a href="#" class="">Layanan</a></li>
@@ -18,30 +18,36 @@
                 <h3 class="text-base-800 text-[36px] font-bold">Transaksi</h3>
             </div>
             <div class="flex flex-col lg:flex-row justify-center mx-auto">
-                <div class="w-[600px] h-[250px] p-8 mr-7 lg:mr-8 rounded-2xl bg-green-500 shadow-lg">
+                <div class="w-[30rem] h-[250px] p-8 mr-7 lg:mr-8 rounded-2xl bg-green-500 shadow-lg">
                     <h3 class="text-xl font-bold pb-5">Apa yang bisa kamu dapatkan?</h3>
                     <p class="text-base text-base-800">Dengan membeli paket layanan Kuliah Karier, kamu bisa dapat banyak hal menarik loh!</p>
                 </div>
-                <div class="h-full mt-7 lg:my-0 lg:w-[392px] max-w-md p-8 rounded-2xl bg-warning shadow-lg">
-                    <h3 class="font-body text-xl ">Paket Jurusan IPA/IPS</h3>
-                    <div class="grid grid-cols-2 pt-6 gap-3">
-                        <p>ID Pesanan</p>
-                        <p>IDPILJUR0123</p>
-                        <p>Metode</p>
-                        <p class="border-b-2">BRI (Transfer)</p>
-                        {{-- <span class="border w-[328px] border-base-200"></span> --}}
-                    </div>
-                    <div class="grid grid-cols-2 pt-6 gap-3">
-                        <p>Harga</p>
-                        <p>Rp110.000</p>
-                        <p>Diskon 50%</p>
-                        <p>-Rp55.000</p>
-                        <span class="border w-[328px] border-base-200"></span>
-                    </div>
-                    <div class="grid grid-cols-2 pt-6 gap-3">
-                        <p>Total Harga</p>
-                        <p>Rp55.000</p>
-                    </div>
+                <div class=" p-8 rounded-2xl bg-light-100 shadow-lg max-w-sm mt-5 lg:mt-0">
+                    <h3 class="text-xl">Paket Penjurusan IPA/IPS</h3>
+                    <table class=" w-full mt-3 auto border-spacing-2 transaction">
+                        <tbody class="text-base-600">
+                            <tr>
+                                <td class="py-2">ID Pesanan</td>
+                                <td class="py-2 text-right">IDPILJUR0123</td>
+                            </tr>
+                            <tr>
+                                <td class="py-2">Metode</td>
+                                <td class="py-2 text-right">BRI (Transfer)</td>
+                            </tr>
+                            <tr>
+                                <td class="py-2">Harga</td>
+                                <td class="py-2 text-right">Rp110.000</td>
+                            </tr>
+                            <tr>
+                                <td class="py-2">Diskon 50%</td>
+                                <td class="py-2 text-right">-Rp55.000</td>
+                            </tr>
+                            <tr>
+                                <td class="py-2">Total Harga</td>
+                                <td class="py-2 text-right font-bold">Rp55.000</td>
+                            </tr>
+                        </tbody>
+                    </table>
                     <div class="mx-auto mt-5 ">
                         <div class="flex flex-col justify-center gap-3 mt-5 !px-0">
                             <x-atoms.button size="lg" status="solid" class="">
@@ -53,15 +59,12 @@
                         </div>
                     </div>
                 </div>
-
-
-                
             </div>
         </div>
 
     </section>
 </x-layouts.full-row>
 
-<div class="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
+{{-- <div class="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
 
-</div>
+</div> --}}
