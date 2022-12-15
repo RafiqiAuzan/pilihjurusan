a64158471727e72d6397da5aefe0050b89df10f4
zenadwim <zenadwimentari@gmail.com>
2022-12-15 11:29:00 +0700

    blank history layout

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('pages.transactions.detailpembayaran');
+    return view('pages.transactions.blankhistory');
 });
 
 // Route::middleware('guest')->group(function () {
