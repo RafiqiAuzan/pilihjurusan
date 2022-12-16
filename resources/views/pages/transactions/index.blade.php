5f691abfe0060e9320c80ad314a4186bdd11e7e7
rafiakbar13 <muhammadrafia13@gmail.com>
2022-12-16 19:38:45 +0700

    added images for transaction step-2

--- resources/views/pages/transactions/index.blade.php
+++ resources/views/pages/transactions/index.blade.php
@@ -5,14 +5,11 @@
         <div class="space-y-8">
             <div class="space-y-5">
                 <div class="flex flex-wrap gap-3">
-                    <x-atoms.button tag="a" status="transparent" href="#"
-                        class="text-base-400 font-normal after:content-['>'] after:pl-3">Layanan
+                    <x-atoms.button tag="a" status="transparent" href="#" class="text-base-400 font-normal after:content-['>'] after:pl-3">Layanan
                     </x-atoms.button>
-                    <x-atoms.button tag="a" status="transparent" href="#"
-                        class="text-base-400 font-normal after:content-['>'] after:pl-3 after:text-base-800">Paket
+                    <x-atoms.button tag="a" status="transparent" href="#" class="text-base-400 font-normal after:content-['>'] after:pl-3 after:text-base-800">Paket
                         Penjurusan IPA/IPS</x-atoms.button>
-                    <x-atoms.button tag="a" status="transparent" href="#"
-                        class="font-normal text-base-800">Transaksi
+                    <x-atoms.button tag="a" status="transparent" href="#" class="font-normal text-base-800">Transaksi
                     </x-atoms.button>
                 </div>
                 <div class="flex items-center gap-4">
@@ -33,27 +30,29 @@
                         <p class="">Dengan membeli paket layanan Kuliah Karier, kamu bisa dapat banyak hal menarik
                             loh!</p>
                         <ul class="mt-2 space-y-2">
-                            <li class="flex items-center gap-3"><i
-                                    class="p-1 bg-green-400 rounded-full text-light-100 fa-2xs fas fa-check"></i>
+                            <li class="flex items-center gap-3"><i class="p-1 bg-green-400 rounded-full text-light-100 fa-2xs fas fa-check"></i>
                                 Workshop</li>
-                            <li class="flex items-center gap-3"><i
-                                    class="p-1 bg-green-400 rounded-full text-light-100 fa-2xs fas fa-check"></i>
+                            <li class="flex items-center gap-3"><i class="p-1 bg-green-400 rounded-full text-light-100 fa-2xs fas fa-check"></i>
                                 Psikotest</li>
                         </ul>
                     </div>
                 </div>
 
                 <style>
-                    input:checked + label {
+                    input:checked+label {
                         border-color: #f06623;
                         border-width: 2px;
                     }
+
                 </style>
 
                 <div class="px-8 py-5 lg:w-[640px] rounded-xl bg-light-100">
                     <h2 class="text-xl font-bold py-2">Paket Penjurusan IPA/IPS</h2>
-                    <div class="my-3" x-data="{ manual: true }"">
-                        <div class="flex justify-center mb-6 ">
+                    {{-- Images --}}
+                    {{-- <img class="w-28 lg:w-full hidden lg:block rounded-3xl aspect-square" src="{{ asset('img/global/stonipe.png') }}"
+                            alt="Frame"> --}}
+                    <div class="my-3" x-data="{ manual: true }">
+                        <div class="flex justify-center mb-6">
                             <div class="relative flex w-full rounded-[36px] bg-base-200 gap-3.5 px-3.5 py-4">
                                 <span class="absolute inset-0 m-1 pointer-events-none" aria-hidden="true">
                                     <span class="absolute inset-0 w-1/2 transition duration-150 ease-in-out transform rounded-[100px] shadow-[0px_1px_8px_rgba(0,0,0,0.12)] bg-light-100" :class="manual ? 'translate-x-0' : 'translate-x-full'"></span>
@@ -71,72 +70,77 @@
                                 <label class="flex p-3 w-full border border-light-400 cursor-pointer rounded-2xl" for="bri">
                                     <div class="flex">
                                         <img src="{{ asset('img/logo/bri.svg') }}" alt="bri" class="w-8 h-8">
-                                        <span class="pl-3 my-auto">BRI (Transfer)</span>
-                                    </div>
-                                </label>
-                            </div>
-                            <div class="space-y-4">
-                                <!-- Payment Option -->
-                                <input class="hidden" type="radio" id="ovo" name="radio">
-                                <label class="flex p-3 w-full border border-light-400 cursor-pointer rounded-2xl" for="ovo">
-                                    <div class="flex">
-                                        <img src="{{ asset('img/logo/ovo.svg') }}" alt="ovo" class="w-8 h-8">
-                                        <span class="pl-3 my-auto">OVO</span>
-                                    </div>
-                                </label>
-                            </div>
-                            <div class="space-y-4">
-                                <!-- Payment Option -->
-                                <input class="hidden" type="radio" id="gopay" name="radio">
-                                <label class="flex p-3 w-full border border-light-400 cursor-pointer rounded-2xl" for="gopay">
-                                    <div class="flex">
-                                        <img src="{{ asset('img/logo/gopay.svg') }}" alt="gopay" class="w-8 h-8">
-                                        <span class="pl-3 my-auto">Gopay</span>
-                                    </div>
-                                </label>
-                            </div>
-                            <div class="space-y-4 pt-4">
-                                <x-atoms.button tag="a" href="transaction2" class="w-full text-center">Selanjutnya
-                                </x-atoms.button>
-                                <x-atoms.button status="outline" class="w-full">Ubah Metode</x-atoms.button>
-                            </div>
-                        </div>
-
-                        <!-- Otomatis form -->
-                        <div x-show="!manual" x-cloak>
-                            <div>
-                                <input type="text" class="flex justify-center text-center bg-light-200 mx-auto h-[7.25rem] w-full" placeholder="{{ __('Fitur saat ini belum tersedia') }}" disabled readonly>
-                            </div>
-                        </div>
-
-                        {{-- <table class="w-full transction-tables">
-                            <tbody class="">
-                                <tr>
-                                    <td>ID Transaksi</td>
-                                    <td>IDPILJUR0123</td>
-                                </tr>
-                                <tr>
-                                    <td>Metode</td>
-                                    <td>BRI (Transfer)</td>
-                                </tr>
-                                <tr>
-                                    <td>Harga</td>
-                                    <td>Rp 110.000</td>
-                                </tr>
-                                <tr>
-                                    <td>Diskon (50%)</td>
-                                    <td>-Rp 55.000</td>
-                                </tr>
-                                <tr>
-                                    <td>Total Harga</td>
-                                    <td class="font-bold">Rp 55.000</td>
-                                </tr>
-                            </tbody>
-                        </table> --}}
-
-                    </div>
+                    <span class="pl-3 my-auto">BRI (Transfer)</span>
                 </div>
+                </label>
             </div>
+            <div class="space-y-4">
+                <!-- Payment Option -->
+                <input class="hidden" type="radio" id="ovo" name="radio">
+                <label class="flex p-3 w-full border border-light-400 cursor-pointer rounded-2xl" for="ovo">
+                    <div class="flex">
+                        <img src="{{ asset('img/logo/ovo.svg') }}" alt="ovo" class="w-8 h-8">
+                        <span class="pl-3 my-auto">OVO</span>
+                    </div>
+                </label>
+            </div>
+            <div class="space-y-4">
+                <!-- Payment Option -->
+                <input class="hidden" type="radio" id="gopay" name="radio">
+                <label class="flex p-3 w-full border border-light-400 cursor-pointer rounded-2xl" for="gopay">
+                    <div class="flex">
+                        <img src="{{ asset('img/logo/gopay.svg') }}" alt="gopay" class="w-8 h-8">
+                        <span class="pl-3 my-auto">Gopay</span>
+                    </div>
+                </label>
+            </div>
+            <div class="space-y-4 pt-4">
+                <x-atoms.button tag="a" href="transaction2" class="w-full text-center">Selanjutnya
+                </x-atoms.button>
+                <x-atoms.button status="outline" class="w-full">Ubah Metode</x-atoms.button>
+            </div>
+        </div>
+
+        <!-- Otomatis form -->
+        <div x-show="!manual" x-cloak>
+            <div>
+                <input type="text" class="flex justify-center text-center bg-light-200 mx-auto h-[7.25rem] w-full" placeholder="{{ __('Fitur saat ini belum tersedia') }}" disabled readonly>
+            </div>
+        </div>
+
+        {{-- <table class="w-full transction-tables">
+            <tbody class="">
+                <tr>
+                    <td>ID Transaksi</td>
+                    <td>IDPILJUR0123</td>
+                </tr>
+                <tr>
+                    <td>Metode</td>
+                    <td>BRI (Transfer)</td>
+                </tr>
+                <tr>
+                    <td>Harga</td>
+                    <td>Rp 150.000</td>
+                </tr>
+                <tr>
+                    <td>Diskon</td>
+                    <td>-Rp 90.000</td>
+                </tr>
+                <tr>
+                    <td>Total Harga</td>
+                    <td class="font-bold">Rp 60.000</td>
+                </tr>
+            </tbody>
+        </table>
+        <div class="space-y-4">
+            <x-atoms.button tag="a" href="/receipt" class="w-full text-center">Selanjutnya
+            </x-atoms.button>
+            <x-atoms.button status="outline" class="w-full">Ubah Metode</x-atoms.button>
+        </div> --}}
+        </div>
+
+        </div>
+        </div>
 
 
         </div>
