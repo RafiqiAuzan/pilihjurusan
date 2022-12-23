bea57e5ea0a2cec5c11b3eae2b16060b1da85ea8
rafiakbar13 <muhammadrafia13@gmail.com>
2022-12-23 09:50:34 +0700

    added pagination

--- resources/views/pages/transactions/transaction-history.blade.php
+++ resources/views/pages/transactions/transaction-history.blade.php
@@ -174,6 +174,47 @@
                             </tbody>
                         </table>
                     </div>
+
+    <div class="flex justify-center lg:justify-end py-4">
+        <a href="#" class="flex items-center justify-center px-4 py-2 mx-1 text-gray-500 capitalize bg-base-200 rounded-md cursor-not-allowed rtl:-scale-x-100 dark:bg-gray-800 dark:text-gray-600">
+        <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
+            <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
+        </svg>
+    </a>
+
+    <a href="#" class=" px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-base-200 rounded-md sm:inline dark:bg-gray-800 dark:text-gray-200 hover:bg-blue-500 dark:hover:bg-blue-500 hover:text-white dark:hover:text-gray-200">
+        1
+    </a>
+
+    <a href="#" class=" px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-base-200 rounded-md sm:inline dark:bg-gray-800 dark:text-gray-200 hover:bg-blue-500 dark:hover:bg-blue-500 hover:text-white dark:hover:text-gray-200">
+        2
+    </a>
+
+    <a href="#" class=" px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-base-200 rounded-md sm:inline dark:bg-gray-800 dark:text-gray-200 hover:bg-blue-500 dark:hover:bg-blue-500 hover:text-white dark:hover:text-gray-200">
+        3
+    </a>
+
+    <a href="#" class=" hidden px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-base-200 rounded-md sm:inline dark:bg-gray-800 dark:text-gray-200 hover:bg-blue-500 dark:hover:bg-blue-500 hover:text-white dark:hover:text-gray-200">
+        4
+    </a>
+
+    <a href="#" class="hidden px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-base-200 rounded-md sm:inline dark:bg-gray-800 dark:text-gray-200 hover:bg-blue-500 dark:hover:bg-blue-500 hover:text-white dark:hover:text-gray-200">
+        5
+    </a>
+    <a href="#" class=" px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-base-200 rounded-md sm:inline dark:bg-gray-800 dark:text-gray-200 hover:bg-blue-500 dark:hover:bg-blue-500 hover:text-white dark:hover:text-gray-200">
+        ...
+    </a>
+    <a href="#" class=" px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-base-200 rounded-md sm:inline dark:bg-gray-800 dark:text-gray-200 hover:bg-blue-500 dark:hover:bg-blue-500 hover:text-white dark:hover:text-gray-200">
+        50
+    </a>
+
+    <a href="#" class="flex items-center justify-center px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-base-200 rounded-md rtl:-scale-x-100 dark:bg-gray-800 dark:text-gray-200 hover:bg-blue-500 dark:hover:bg-blue-500 hover:text-white dark:hover:text-gray-200">
+        <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
+            <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
+        </svg>
+    </a>
+</div>
+
                 </div>
             </div>
         </div>
