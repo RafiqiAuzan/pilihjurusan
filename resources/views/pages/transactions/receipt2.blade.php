254306a2099cdfa8b9b0d6296e4e798e9fff8e75
RafiqiAuzan <rafikids32@gmail.com>
2022-12-24 22:02:32 +0700

    added modal from payment confirmation

--- resources/views/pages/transactions/receipt2.blade.php
+++ resources/views/pages/transactions/receipt2.blade.php
@@ -2,7 +2,7 @@
 
     {{-- Transaction --}}
     <section class="pt-8">
-        <div class="space-y-8">
+        <div class="space-y-8" x-data="{ 'exitConfirmation': false }" @keydown.escape="exitConfirmation = false">
             <div class="space-y-5">
                 <div class="flex flex-wrap gap-3">
                     <x-atoms.button tag="a" status="transparent" href="#"
@@ -19,9 +19,23 @@
                     </x-atoms.button>
                 </div>
                 <div class="flex items-center gap-8">
-                    <x-atoms.button tag="a" href="rincian" status="transparent" class="cursor-pointer text-base-800">
+                    <x-atoms.button tag="a" status="transparent" class="cursor-pointer text-base-800" @click="exitConfirmation = true">
                         <i class="fas fa-arrow-left fa-xl"></i>
                     </x-atoms.button>
+                    <!-- Modal -->
+                    <div class="fixed inset-0 z-30 flex items-center justify-center bg-dark-100 bg-opacity-50 p-8" x-show="exitConfirmation" x-cloak>
+                        <!-- Modal inner -->
+                        <div class="max-w-md px-7 py-4 mx-auto text-left bg-light-100 rounded-3xl shadow-lg" @click.away="exitConfirmation = false" x-transition:enter="motion-safe:ease-out duration-300" x-transition:enter-start="opacity-0 scale-90" x-transition:enter-end="opacity-100 scale-100">
+                            <div>
+                                <h2 class="text-2xl font-bold">Apakah kamu yakin?</h2>
+                                <p class="text-base-500 my-4">Apakah kamu yakin untuk keluar dari halaman pembayaran dan kembali ke halaman sebelumnya? Kamu dapat upload bukti pembayaran kembali lewat halaman Riwayat Transaksi lalu buka detail transaksi.</p>
+                                <div class="flex flex-row text-center mx-auto justify-center gap-x-3">
+                                    <x-atoms.button status="outline" @click="exitConfirmation = false" class="border-0">Belum Yakin</x-atoms.button>
+                                    <x-atoms.button href="/" @click="exitConfirmation = true">Yakin</x-atoms.button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     <h1 class="text-4xl font-bold">Konfirmasi Pembayaran</h1>
                 </div>
             </div>
@@ -54,8 +68,22 @@
                     </x-atoms.button>
                 </div>
             </form>
-            <div class="flex justify-end gap-4">
-                <x-atoms.button status="solid" class="">Konfirmasi</x-atoms.button>
+            <div class="flex justify-end gap-4" x-data="{ 'Confirmation': false }" @keydown.escape="Confirmation = false">
+                <x-atoms.button status="solid" class="" @click="Confirmation = true">Konfirmasi</x-atoms.button>
+                <!-- Modal -->
+                <div class="fixed inset-0 z-30 flex items-center justify-center bg-dark-100 bg-opacity-50 p-8" x-show="Confirmation" x-cloak>
+                    <!-- Modal inner -->
+                    <div class="max-w-md px-7 py-4 mx-auto text-left bg-light-100 rounded-3xl shadow-lg" @click.away="Confirmation = false" x-transition:enter="motion-safe:ease-out duration-300" x-transition:enter-start="opacity-0 scale-90" x-transition:enter-end="opacity-100 scale-100">
+                        <div>
+                            <h2 class="text-2xl font-bold">Apakah kamu yakin?</h2>
+                            <p class="text-base-500 my-4">Kamu hanya bisa mengupload ulang satu kali setelah ini lewat halaman Riwayat Transaksi lalu akses detail transaksi, dan jika file yang telah diunggah sudah Minju approve, maka kamu nggak bisa mengubahnya lagi ya!</p>
+                            <div class="flex flex-row text-center mx-auto justify-center gap-x-3">
+                                <x-atoms.button status="outline" @click="Confirmation = false" class="border-0">Belum Yakin</x-atoms.button>
+                                <x-atoms.button href="riwayat-transaksi" @click="Confirmation = true">Yakin</x-atoms.button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
 
